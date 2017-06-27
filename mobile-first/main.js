d87b6c9edd602b637542398d0e23f68497fc71bc
DESKTOP_PC\Benjamin_Mulligan <benmulligan49@hotmail.co.uk>
2017-06-27 19:38:20 +0100

    Add scroll Function for menu

--- mobile-first/main.js
+++ mobile-first/main.js
@@ -6,10 +6,12 @@ function openNav() {
   document.getElementById("mySidenav").style.display = "block";
   document.getElementById("open").style.visibility = "hidden";
   document.getElementById("navSection").style.height = "40%";
+  document.body.style.overflow = "hidden";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.display = "none";
   document.getElementById("open").style.visibility = "visible";
   document.getElementById("navSection").style.height = "auto";
+  document.body.style.overflow = "scroll";
 }
